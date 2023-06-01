afe86b8e116462771e9ccc62294af72e411f9b18
FrancescoAceto <acetof02@gmail.com>
2023-06-01 22:24:26 +0200

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,49 @@
+<script>
+import axios from 'axios';
+
+import ProjectItem from './ProjectItem.vue';
+
+export default {
+    name: 'AppMain',
+
+    data() {
+     return {
+        projects: []
+     }
+     
+    },
+
+    components: {
+        ProjectItem,
+    },
+
+    mounted() {
+        this.getProjects();
+    },
+
+    methods: {
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+                console.log(response.data.results);
+                this.projects = response.data.results;
+            })
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Boolfolio</h1>
+
+        <div class="row">
+            <div v-for="project in projects" class="col-4">
+                <ProjectItem :project="project"></ProjectItem>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'ProjectItem',
+    
+    data() {
+        return {
+
+        }
+    },
+
+    props:{
+        project: Object,
+    },
+
+    computed: {
+        description() {
+            return
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        {{project.title}}
+    </div>
+
+    <p>
+        {{project.description}}
+    </p>
+</template>
